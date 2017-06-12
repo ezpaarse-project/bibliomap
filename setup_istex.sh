ed8f9837350583ec182fe5b937b3ea3bebcb32cb
sfmax54 <narko_54@hotmail.fr>
2017-06-12 13:52:41 +0200

    Update to work with docker images

--- setup_istex.sh
+++ setup_istex.sh
@@ -1,6 +1,5 @@
 export BBV_INDEX=index-istex.html
 export BBV_JSFILE=bibliomap-istex.js
 export BBH_LOG_FOLDER=demo-logs/istex
-export BBE_EZPAARSE_PREDEF=00-fr-istex
-export BBH_LOGSTREAMS=_ISTEX_
+export NODE_APP_INSTANCE=istex
 
