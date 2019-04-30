43cf98ca5387ab9f4ae459a99c9896d9f38a688c
wilmouths <wilmouthsteven@gmail.com>
2019-04-30 11:24:21 +0200

    Adding  in setup file

--- setup_cnrs.sh
+++ setup_cnrs.sh
@@ -2,3 +2,4 @@ export BBV_INDEX=index-cnrs.html
 export BBV_JSFILE=bibliomap-cnrs.js
 export BBH_LOG_FOLDER=demo-logs/cnrs
 export NODE_APP_INSTANCE=cnrs
+export BBH_DEMO_DELAY=1000
--- setup_istex.sh
+++ setup_istex.sh
@@ -2,3 +2,4 @@ export BBV_INDEX=index-istex.html
 export BBV_JSFILE=bibliomap-istex.js
 export BBH_LOG_FOLDER=demo-logs/istex
 export NODE_APP_INSTANCE=istex
+export BBH_DEMO_DELAY=1000
