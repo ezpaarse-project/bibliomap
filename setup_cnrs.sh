ed5293bdf4e12128738047c350adba10a55a23af
wilmouths <wilmouthsteven@gmail.com>
2019-04-30 11:38:01 +0200

    Adding BBE_EZPAARSE_PREDEF in setup file

--- setup_cnrs.sh
+++ setup_cnrs.sh
@@ -3,3 +3,4 @@ export BBV_JSFILE=bibliomap-cnrs.js
 export BBH_LOG_FOLDER=demo-logs/cnrs
 export NODE_APP_INSTANCE=cnrs
 export BBH_DEMO_DELAY=1000
+export BBE_EZPAARSE_PREDEF=00-fr-bibcnrs
--- setup_istex.sh
+++ setup_istex.sh
@@ -3,3 +3,4 @@ export BBV_JSFILE=bibliomap-istex.js
 export BBH_LOG_FOLDER=demo-logs/istex
 export NODE_APP_INSTANCE=istex
 export BBH_DEMO_DELAY=1000
+export BBE_EZPAARSE_PREDEF=00-fr-istex
