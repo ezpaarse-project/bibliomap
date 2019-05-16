cb77dad0d225677c00c9af0b41e734bfd23d3d9f
Yannick Schurter <yannick.schurter@gmail.com>
2019-05-16 15:52:12 +0200

    replace DEMO_DELAY with LOGS_PER_SECOND

--- setup_cnrs.sh
+++ setup_cnrs.sh
@@ -2,6 +2,6 @@ export BBV_INDEX=cnrs
 export BBV_JSFILE=bibliomap-cnrs.js
 export BBH_LOG_FOLDER=demo-logs/cnrs
 export NODE_APP_INSTANCE=cnrs
-export BBH_DEMO_DELAY=1000
+export BBH_LOGS_PER_SECOND=8
 export DEBUG=true
 export BBE_EZPAARSE_PREDEF=00-fr-bibcnrs
--- setup_istex.sh
+++ setup_istex.sh
@@ -2,5 +2,5 @@ export BBV_INDEX=istex
 export BBV_JSFILE=bibliomap-istex.js
 export BBH_LOG_FOLDER=demo-logs/istex
 export NODE_APP_INSTANCE=istex
-export BBH_DEMO_DELAY=1000
+export BBH_LOGS_PER_SECOND=2
 export BBE_EZPAARSE_PREDEF=00-fr-istex-1
