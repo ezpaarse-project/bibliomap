4a1879479f0f2e905b18af13b22bce597a237c2b
leofelix <leo.felix@inist.fr>
2019-05-16 08:58:32 +0200

    update environnement variables

--- setup_cnrs.sh
+++ setup_cnrs.sh
@@ -1,6 +1,7 @@
-export BBV_INDEX=index-cnrs.html
+export BBV_INDEX=cnrs
 export BBV_JSFILE=bibliomap-cnrs.js
 export BBH_LOG_FOLDER=demo-logs/cnrs
 export NODE_APP_INSTANCE=cnrs
 export BBH_DEMO_DELAY=1000
+export DEBUG=true
 export BBE_EZPAARSE_PREDEF=00-fr-bibcnrs
