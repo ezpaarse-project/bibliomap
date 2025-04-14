0e2e3546a393fe7dc8bc1bc1f67d0af5b9622535
Zack Pedretti <zackpedretti52@gmail.com>
2025-04-14 11:44:58 +0200

    feat: Changed how timer and time comparison works, timer can begin at the beginning of the day or the ftime of the first log

--- services/enricher/selected-logs-reader.js
+++ services/enricher/selected-logs-reader.js
@@ -21,87 +21,86 @@ class SelectedLogsReader extends EventEmitter {
 
     this.server = net.createServer();
 
-    this.emit('+node', '', streamName);
-
     const stream = fs.createReadStream(filePath, { encoding: 'utf-8', highWaterMark: 2048 });
 
     const rl = readline.createInterface({
       input: stream,
-      output: process.stdout,
-      terminal: false
     });
 
     const lineQueue = [];
     let paarseQueue = [];
     let timer = 0;
-    let firstLogTime;
+    let firstLogDate;
+    let loading = true;
 
-    const multiplier = 1;
+    const multiplier = 10;
     const baseTime = 1000;
+    const startAtFirstLog = true;
 
     rl.on('line', async (line) => {
       if (line) {
-        //console.log("LINE:", line);
-
         paarseQueue.push(line);
 
         if (Math.max(lineQueue.length, paarseQueue.length) > 2) stream.pause();
         else stream.resume();
-        
+
         const response = await fetch(config.ezpaarse.url, {
           method: 'POST',
-          headers: {
-            "Accept": "application/jsonstream",
-            "Double-Click-Removal": "false",
-            "crossref-enrich": "false",
-            "ezPAARSE-Buffer-Size": 0,
-            "ezPAARSE-Predefined-Settings": "00-fr-bibcnrs"
+          headers: { // CANNOT SEEM TO USE THE HEADERS FROM THE CONFIG! HELP!!!
+            Accept: 'application/jsonstream',
+            'Double-Click-Removal': 'false',
+            'crossref-enrich': 'false',
+            'ezPAARSE-Predefined-Settings': 'bibliomap'
           },
           body: line
         });
 
         const responseText = await response.text();
 
-        if(!response.ok) {
-          console.log("ERROR:", response.status, response.statusText);
+        if (!response.ok) {
+          console.error("ERROR:", response.status, response.statusText);
         }
 
-        else if(responseText) {
+        else if (responseText) {
           const json = JSON.parse(responseText.trim());
           const date = new Date(json.datetime);
-          if(!firstLogTime) firstLogTime = date;
-          lineQueue.push({date: date, line: line});
-          console.log("DATE AND LINE:", {date: date, line: line})
+          if (!firstLogDate) firstLogDate = date;
+          lineQueue.push({ date: date, line: line });
         }
 
         paarseQueue = paarseQueue.filter(l => l !== line);
       }
-    })
+    });
 
     const interval = setInterval(() => {
-      timer += baseTime * multiplier;
-      if(Math.max(lineQueue.length, paarseQueue.length) > 2) stream.pause();
+      if (loading) console.log("[debug] loading");
+      if (Math.max(lineQueue.length, paarseQueue.length) > 2) stream.pause();
       else stream.resume();
-      if (lineQueue.length > 1) {
-        
-        const line = lineQueue[0];
-        if (line.date.getTime() - firstLogTime.getTime() < timer) {
-          console.log("Line date:", line.date, "first log date:", firstLogTime, "difference:", line.date.getTime() - firstLogTime.getTime(), "timer:", timer);
-          this.parseLine(lineQueue.shift().line, streamName);
-        }
-        
+      if (!firstLogDate) return;
+
+      if (loading){
+        loading = false;
+        timer = firstLogDate.getTime();
+        if (!startAtFirstLog) timer = (new Date(timer).setHours(0, 0, 0, 0)); // Set to the beginning of the day
+      }
+      
+      timer += baseTime;
+      console.log("[debug] timer:", new Date(timer));
+
+      if (!lineQueue.length) return;
+      const line = lineQueue[0];
+
+      if (line.date.getTime() <= timer) {
+        console.log("[debug] line date:", line.date);
+        this.parseLine(lineQueue.shift().line, streamName);
       }
     }, baseTime / multiplier);
 
     return cb();
   }
 
-  /*
-  *  line: [+log, streamName, node, type, log ("140.93.9.33 BKmQCaWyWCHfASb - anonymous@cnrs.fr_O_CNRS_I_DS61_OU_UMR0000 [21/Nov/2016:16:59:53 +0000] "GET http://ac.els-cdn.com:80/S0025540816301830/1-s2.0-S0025540816301830-main.pdf?_tid=eb7585fc-b00b-11e6-9bb6-00000aacb362&acdnat=1479747772_050f425fa2f4909faefe714448bd2ed6 HTTP/1.1" 206 606834 ins2i")]
-  *  +log: new bubble
-  */
   parseLine = (line, streamName) => {
-    if(!line) return;
+    if (!line) return;
     this.emit('+log', streamName.toUpperCase(), 'bibliomap', 'info', line.trim());
   }
 }
