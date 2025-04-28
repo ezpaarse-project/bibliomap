65941510849328ce3e214006acc64493608f751e
Zack Pedretti <zackpedretti52@gmail.com>
2025-04-28 11:04:08 +0200

    fix: Remove additional top left zoom buttons

--- services/vue-viewer/src/components/WorldMap.vue
+++ services/vue-viewer/src/components/WorldMap.vue
@@ -18,10 +18,10 @@
     map = L.map('map', {
       minZoom: 3,
       maxZoom: 9,
+      zoomControl: false,
     }).setView([props.defaultX || 46.603354, props.defaultY || 1.888334], props.defaultZoom || 6);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
