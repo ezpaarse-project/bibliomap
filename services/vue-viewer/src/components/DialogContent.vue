564309ae22ba9fa0484db20bc3ecf4a565a4b7ad
Zack Pedretti <zackpedretti52@gmail.com>
2025-05-13 08:15:31 +0200

    fix: Responsive dialog

--- services/vue-viewer/src/components/DialogContent.vue
+++ services/vue-viewer/src/components/DialogContent.vue
@@ -2,7 +2,7 @@
   <div class="drawer-elements-container">
     <div>
       <v-list-item>
-        <v-card>
+        <v-card :flat="true">
           <v-card-title
             class="text-h4 justify-center"
             style="white-space: normal; overflow: visible;"
@@ -13,7 +13,7 @@
         </v-card>
       </v-list-item>
       <v-list-item>
-        <v-card>
+        <v-card :flat="true">
           <a href="https://bib.cnrs.fr/">
             <img class="bibcnrs-img" src="@/assets/bibCnrs.png">
           </a>
@@ -26,23 +26,29 @@
         </v-card>
       </v-list-item>
       <v-list-item>
-        <div class="ez-logos">
-          <a href="http://ezpaarse.couperin.org/"><img src="@/assets/logo-ezpaarse.png"></a>
-          <a href="http://analogist.couperin.org/"><img src="@/assets/logo-analogist.png"></a>
-        </div>
-        <v-card-text>
-          Les visualisations disponibles dans <a href="https://github.com/ezpaarse-project/bibliomap">BiblioMap</a> sont rendues possible par l’utilisation d’<a href="http://ezpaarse.couperin.org/">ezPAARSE</a>, un <a href="https://github.com/ezpaarse-project/ezpaarse">logiciel libre</a> développé conjointement par <a href="http://www.couperin.org/presentation">COUPERIN</a>, l'<a href="http://www.inist.fr/">INIST-CNRS</a> et l’<a href="http://www.univ-lorraine.fr/">Université de Lorraine</a> comme site pilote. <a href="http://analogist.couperin.org/">AnalogIST</a> est le portail collaboratif de l’Enseignement Supérieur et Recherche dédié à la problématique de la mesure des usages des ressources électroniques. Les développements associés à cette collaboration sont disponibles sous forme de logiciel libre au sein d’<a href="https://github.com/ezpaarse-project/">ezPAARSE-project</a>.
-        </v-card-text>
+        <v-card :flat="true">
+          <div class="ez-logos">
+            <a href="http://ezpaarse.couperin.org/"><img src="@/assets/logo-ezpaarse.png"></a>
+            <a href="http://analogist.couperin.org/"><img src="@/assets/logo-analogist.png"></a>
+          </div>
+          <v-card-text>
+            Les visualisations disponibles dans <a href="https://github.com/ezpaarse-project/bibliomap">BiblioMap</a> sont rendues possible par l’utilisation d’<a href="http://ezpaarse.couperin.org/">ezPAARSE</a>, un <a href="https://github.com/ezpaarse-project/ezpaarse">logiciel libre</a> développé conjointement par <a href="http://www.couperin.org/presentation">COUPERIN</a>, l'<a href="http://www.inist.fr/">INIST-CNRS</a> et l’<a href="http://www.univ-lorraine.fr/">Université de Lorraine</a> comme site pilote. <a href="http://analogist.couperin.org/">AnalogIST</a> est le portail collaboratif de l’Enseignement Supérieur et Recherche dédié à la problématique de la mesure des usages des ressources électroniques. Les développements associés à cette collaboration sont disponibles sous forme de logiciel libre au sein d’<a href="https://github.com/ezpaarse-project/">ezPAARSE-project</a>.
+          </v-card-text>
+        </v-card>
       </v-list-item>
       <v-list-item>
-        <v-card-text>Remarque : la localisation des consultations apparaissant sur la carte est volontairement approximative.</v-card-text>
+        <v-card :flat="true">
+          <v-card-text>Remarque : la localisation des consultations apparaissant sur la carte est volontairement approximative.</v-card-text>
+        </v-card>
       </v-list-item>
       <v-list-item>
-        <div class="org-logos">
-          <a href="http://www.couperin.org/"><img src="@/assets/logo-couperin.jpg"></a>
-          <a href="http://www.cnrs.fr/"><img src="@/assets/logo.svg"></a>
-          <a href="http://www.univ-lorraine.fr/"><img src="@/assets/logo-universite-de-lorraine.png"></a>
-        </div>
+        <v-card :flat="true">
+          <div class="org-logos">
+            <a href="http://www.couperin.org/"><img src="@/assets/logo-couperin.jpg"></a>
+            <a href="http://www.cnrs.fr/"><img src="@/assets/logo.svg"></a>
+            <a href="http://www.univ-lorraine.fr/"><img src="@/assets/logo-universite-de-lorraine.png"></a>
+          </div>
+        </v-card>
       </v-list-item>
     </div>
   </div>
--- services/vue-viewer/src/components/DialogDrawer.vue
+++ services/vue-viewer/src/components/DialogDrawer.vue
@@ -5,7 +5,8 @@
     elevation="5"
     :location="drawerLocation"
     :permanent="true"
-    width="600"
+    style="max-width: 100vw;"
+    :width="width"
   >
     <v-btn :flat="true" icon="mdi-close" @click.stop="drawer = false" />
     <DialogContent />
@@ -28,4 +29,5 @@
   const usingPhone = window.innerWidth <= 768;
   const drawer = ref(!usingPhone);
   const drawerLocation = (props.position ?? 'left') as 'left' | 'top' | 'bottom' | 'start' | 'end' | 'right' | undefined;
+  const width = Math.min(600, window.innerWidth);
 </script>
