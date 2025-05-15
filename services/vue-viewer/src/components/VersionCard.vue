38b7ac7713da09a55474eafd178846593e703608
Zack Pedretti <zackpedretti52@gmail.com>
2025-05-15 16:01:00 +0200

    feat: Version card i18n

--- services/vue-viewer/src/components/VersionCard.vue
+++ services/vue-viewer/src/components/VersionCard.vue
@@ -3,11 +3,16 @@
     <v-card density="comfortable" :flat="true" height="65" width="160">
       <v-card-title class="justify-center text-body-1 pb-0">Bibliomap 6.0</v-card-title>
       <v-card-text class="justify-center text-caption pt-0">
-        Connecté à ezPAARSE
+        {{ t('version-card.connection') }}
       </v-card-text>
     </v-card>
   </div>
 </template>
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n';
+
+  const { t } = useI18n();
+</script>
 <style lang="scss">
   .card-version-container{
     position: fixed;
