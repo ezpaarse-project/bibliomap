1e9d618085c376573bf60bb328da05cc0582d1b8
Zack Pedretti <zackpedretti52@gmail.com>
2025-05-16 16:01:54 +0200

    feat: Show file type count when a portal is hovered

--- services/vue-viewer/src/components/drawer-modules/Portals.vue
+++ services/vue-viewer/src/components/drawer-modules/Portals.vue
@@ -1,17 +1,61 @@
 <template>
   <div class="portals-component">
     <v-list-item v-for="(value, key) in config.portals" :key="key">
-      <div class="portal-list-item">
-        <div class="portal-container">
-          <img v-if="value.icon" :src="getIconUrl(value.icon)">
-          <div>
-            <h3>{{ t(`drawer-custom.portals.${key}.title`) }}</h3>
-            <p>{{ t(`drawer-custom.portals.${key}.subtitle`) }}</p>
+      <v-tooltip
+        v-if="counts[key] && Object.keys(counts[key]).length > 0"
+        location="right"
+      >
+        <template #activator="{ props }">
+          <div class="portal-list-item" v-bind="props">
+            <div class="portal-container">
+              <img v-if="value.icon" :src="getIconUrl(value.icon)">
+              <div>
+                <h3>{{ t(`drawer-custom.portals.${key}.title`) }}</h3>
+                <p>{{ t(`drawer-custom.portals.${key}.subtitle`) }}</p>
+              </div>
+            </div>
+            <v-chip
+              :color="config.portals[key].color"
+              variant="flat"
+            >
+              {{ Object.values(counts[key]).reduce((a, b) => a + b, 0) }}
+            </v-chip>
+          </div>
+        </template>
+
+        <!-- Tooltip content -->
+        <div>
+          <div
+            v-for="(val, subKey) in counts[key]"
+            :key="subKey"
+          >
+            {{ subKey }}: {{ val }}
           </div>
         </div>
-        <v-chip v-if="counts[key] != null" :color="config.portals[key].color" variant="flat">{{ counts[key] }}</v-chip>
-      </div>
+      </v-tooltip>
+
+      <!-- Fallback when no tooltip is shown -->
+      <template v-else>
+        <div class="portal-list-item">
+          <div class="portal-container">
+            <img v-if="value.icon" :src="getIconUrl(value.icon)">
+            <div>
+              <h3>{{ t(`drawer-custom.portals.${key}.title`) }}</h3>
+              <p>{{ t(`drawer-custom.portals.${key}.subtitle`) }}</p>
+            </div>
+          </div>
+          <v-chip
+            v-if="counts[key]"
+            :color="config.portals[key].color"
+            variant="flat"
+          >
+            {{ Object.values(counts[key]).reduce((a, b) => a + b, 0) }}
+          </v-chip>
+        </div>
+      </template>
     </v-list-item>
+
+
   </div>
 </template>
 
@@ -25,18 +69,18 @@
   const config = ref(useViewerConfigStore().config);
   const { t } = useI18n();
 
-  const counts = reactive({} as Record<string, number>);
+  const counts = reactive({} as Record<string, Record<string, number>>);
 
   Object.keys(config.value.portals).forEach(key => {
-    counts[key] = 0;
+    counts[key] = {};
   });
 
   const io = useSocketStore().getSocket();
   io?.on('log', (log: Log) => {
     if (usePlatformFilterStore().getFilter() && log.platform_name && !((usePlatformFilterStore().getFilter().toUpperCase().includes(log.platform_name.toUpperCase()) || log.platform_name.toUpperCase().includes(usePlatformFilterStore().getFilter().toUpperCase())))) return;
-    if (log.ezproxyName && Object.keys(counts).includes(log.ezproxyName.toUpperCase())) {
-      counts[log.ezproxyName.toUpperCase()] += 1;
-    }
+    if (!log.ezproxyName || !Object.keys(counts).includes(log.ezproxyName.toUpperCase()) || !log.mime) return;
+    if (!counts[log.ezproxyName.toUpperCase()][log.mime]) counts[log.ezproxyName.toUpperCase()][log.mime] = 0;
+    counts[log.ezproxyName.toUpperCase()][log.mime] += 1;
   });
 
   const getIconUrl = (iconName: string): string => {
