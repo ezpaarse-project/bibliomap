29af39af231c5ffc1782fb0d542a0cf360c04871
Zack Pedretti <zackpedretti52@gmail.com>
2025-07-09 13:58:09 +0200

    fix: Config

--- setup_replay.sh
+++ setup_replay.sh
@@ -1,6 +1,5 @@
 export REPLAY_FILE_PATHS="vpportail.in2p3.ezproxy.2020.03.08.ec.csv,vpportail.inc.ezproxy.2020.03.08.ec.csv,vpportail.inee.ezproxy.2020.03.08.ec.csv,vpportail.inp.ezproxy.2020.03.08.ec.csv,vpportail.ins2i.ezproxy.2020.03.08.ec.csv,vpportail.insb.ezproxy.2020.03.08.ec.csv,vpportail.inshs.ezproxy.2020.03.08.ec.csv,vpportail.insis.ezproxy.2020.03.08.ec.csv,vpportail.insmi.ezproxy.2020.03.08.ec.csv,vpportail.insu.ezproxy.2020.03.08.ec.csv"
-export REPLAY_START_DAY="2020-03-08"
-export REPLAY_START_TIME="07:00:00+01:00"
-export REPLAY_MULTIPLIER=60
+export REPLAY_START_DATETIME="2020-03-08T11:00:00+01:00"
+export REPLAY_MULTIPLIER=10
 export REPLAY_MODE=true
 export REPLAY_DURATION=1
