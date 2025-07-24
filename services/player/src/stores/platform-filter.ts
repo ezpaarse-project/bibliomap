88c24749b8aa3eb342b0ab830d58b7c2af9197c3
Zack Pedretti <zackpedretti52@gmail.com>
2025-07-24 15:31:51 +0200

    refactor: Made function clearer

--- services/player/src/stores/platform-filter.ts
+++ services/player/src/stores/platform-filter.ts
@@ -4,7 +4,13 @@ export const usePlatformFilterStore = defineStore('platform-filter', () => {
   const filter = ref('');
 
   function isNameOkay (name: string) {
-    return !filter.value || (filter.value && name && (filter.value.toUpperCase().includes(name.toUpperCase()) || name.toUpperCase().includes(filter.value.toUpperCase())));
+    if (!filter.value) return true;
+    if (!name) return false;
+
+    const nameUpper = name.toUpperCase();
+    const filterUpper = filter.value.toUpperCase();
+
+    return filterUpper.includes(nameUpper) || nameUpper.includes(filterUpper);
   }
 
   return {
