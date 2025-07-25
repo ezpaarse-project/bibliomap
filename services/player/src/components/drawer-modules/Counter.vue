7a7b50421d867afb488fe5d4824871b229c72321
Zack Pedretti <zackpedretti52@gmail.com>
2025-07-25 08:30:53 +0200

    refactor: using vuetify grid system instead of css

--- services/player/src/components/drawer-modules/Counter.vue
+++ services/player/src/components/drawer-modules/Counter.vue
@@ -2,37 +2,51 @@
   <v-card-title>
     {{ t('drawer.counter.title') }}
   </v-card-title>
-  <div class="counter-container">
-    <v-tooltip
-      v-for="mime in mimes"
-      :key="mime.name"
-      location="top"
-      :text="t('drawer.counter.tooltips.file-type-consultations', {
-        n: countStore.getCountOfMime(mime.name),
-        type: mime.name
-      })"
-    >
-      <template
-        #activator="{ props }"
+
+  <v-container fluid>
+    <v-row align="center" dense justify="space-evenly">
+      <v-col
+        v-for="mime in mimes"
+        :key="mime.name"
+        cols="auto"
       >
-        <v-badge :content="countStore.getCountOfMime(mime.name) as number || 0">
-          <v-chip v-bind="props" :color="mime.color" variant="flat">
-            {{ mime.name }}
-          </v-chip>
-        </v-badge>
-      </template>
-    </v-tooltip>
-    <div>
-      <v-tooltip v-if="counterProps.showTotal" location="top" :text="t('drawer.counter.tooltips.total-consultations', {n: countStore.getTotalCount()})">
-        <template #activator="{ props }">
-          <v-badge :content="countStore.getTotalCount()">
-            <v-chip v-bind="props">{{ t('drawer.counter.total') }}</v-chip>
-          </v-badge>
-        </template>
-      </v-tooltip>
-    </div>
-  </div>
+        <v-tooltip
+          location="top"
+          :text="t('drawer.counter.tooltips.file-type-consultations', {
+            n: countStore.getCountOfMime(mime.name),
+            type: mime.name
+          })"
+        >
+          <template #activator="{ props }">
+            <v-badge :content="countStore.getCountOfMime(mime.name) as number || 0">
+              <v-chip v-bind="props" :color="mime.color" variant="flat">
+                {{ mime.name }}
+              </v-chip>
+            </v-badge>
+          </template>
+        </v-tooltip>
+      </v-col>
+
+      <v-col v-if="counterProps.showTotal" cols="auto">
+        <v-tooltip
+          location="top"
+          :text="t('drawer.counter.tooltips.total-consultations', {
+            n: countStore.getTotalCount()
+          })"
+        >
+          <template #activator="{ props }">
+            <v-badge :content="countStore.getTotalCount()">
+              <v-chip v-bind="props">
+                {{ t('drawer.counter.total') }}
+              </v-chip>
+            </v-badge>
+          </template>
+        </v-tooltip>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 
 <script setup lang='ts'>
   import { useEcCountStore } from '@/stores/ec-count';
@@ -48,15 +62,3 @@
 
   const { shownMimes: mimes } = storeToRefs(useMimeStore())
 </script>
-
-<style lang="scss">
-  .counter-container{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-evenly;
-    margin: 1rem .5rem;
-    flex-wrap: wrap;
-    gap: .5rem;
-  }
-</style>
