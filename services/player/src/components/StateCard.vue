60100d972437ae6c15a78678783af2d5a011d138
Zack Pedretti <zackpedretti52@gmail.com>
2025-07-25 13:52:59 +0200

    refactor: Simpler code

--- services/player/src/components/StateCard.vue
+++ services/player/src/components/StateCard.vue
@@ -1,16 +1,11 @@
 <template>
-  <div class="card-container">
-    <PlayingCard v-if="state === PlayState.PLAYING" />
-    <PauseCard v-if="state === PlayState.PAUSED" />
-    <StoppedCard v-if="state === PlayState.STOPPED" />
-    <LoadingCard v-if="state === PlayState.LOADING" />
-  </div>
+  <PlayingCard v-if="state === PlayState.PLAYING" />
+  <PauseCard v-if="state === PlayState.PAUSED" />
+  <StoppedCard v-if="state === PlayState.STOPPED" />
+  <LoadingCard v-if="state === PlayState.LOADING" />
 </template>
 
 <script setup lang='ts'>
   import { PlayState, usePlayStateStore } from '@/stores/play-state.ts';
-  import PlayingCard from '@/components/state-cards/PlayingCard.vue';
-  import PauseCard from '@/components/state-cards/PauseCard.vue';
-
   const { state } = storeToRefs(usePlayStateStore());
 </script>
