6f7218c51956e62681129a4c8560f4e6581c424e
Zack Pedretti <zackpedretti52@gmail.com>
2025-08-01 14:31:38 +0200

    feat: Better looking player

--- services/player/src/components/information-components/InformationContent.vue
+++ services/player/src/components/information-components/InformationContent.vue
@@ -1,5 +1,8 @@
 <template>
-  <div />
+  <v-card flat>
+    <v-card-title>BiblioMap Player</v-card-title>
+    <v-card-text><a href="https://github.com/ezpaarse-project/bibliomap/tree/master/services/harvester/demo-logs/cnrs" target="_blank">Example files</a></v-card-text>
+  </v-card>
 </template>
 <!-- This component is the content of the information drawer -->
 <!-- You need to change it in order to display your customized information -->
