749f06dd440757848599bd866105409aa9a126a9
Zack Pedretti <zackpedretti52@gmail.com>
2025-08-01 15:54:37 +0200

    fix: Fixed demo files information

--- services/player/src/components/information-components/InformationContent.vue
+++ services/player/src/components/information-components/InformationContent.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card flat>
     <v-card-title>BiblioMap Player</v-card-title>
-    <v-card-text><a href="https://github.com/ezpaarse-project/bibliomap/tree/master/services/harvester/demo-logs/cnrs" target="_blank">Example files</a></v-card-text>
+    <v-card-text><a href="https://github.com/ezpaarse-project/bibliomap/tree/development/services/player/demo-files" target="_blank">Example files</a></v-card-text>
   </v-card>
 </template>
 <!-- This component is the content of the information drawer -->
