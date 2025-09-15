1f25def3fa7e22815160aebe1a73dc2bfd8c9e86
felixleo22 <leofelixpro22@gmail.com>
2025-09-15 15:30:38 +0200

    fix: import

--- services/player/src/pages/index.vue
+++ services/player/src/pages/index.vue
@@ -28,6 +28,7 @@
 <script lang="ts" setup>
   import Drawer from '@/components/menu/Drawer.vue';
   import WorldMap from '@/components/map/WorldMap.vue';
+  import SettingsDialog from '@/components/fab/settings/Dialog.vue';
   import { onBeforeUnmount, onMounted } from 'vue';
   import { PlayState, usePlayStateStore } from '@/stores/play-state';
   import { usePlayerFileStore } from '@/stores/player-file';
