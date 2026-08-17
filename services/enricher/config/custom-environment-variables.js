337d17fb23d8d5fb811e1f719dc759672e105faf
felixleo22 <leofelixpro22@gmail.com>
2026-08-17 15:34:42 +0200

    feat: add env variable for redis

--- services/enricher/config/custom-environment-variables.js
+++ services/enricher/config/custom-environment-variables.js
@@ -2,4 +2,10 @@ module.exports = {
   mode: 'MODE',
   port: 'PORT',
   ezpaarseURL: 'EZPAARSE_URL',
+  redis: {
+    host: 'REDIS_HOST',
+    port: 'REDIS_PORT',
+    password: 'REDIS_PASSWORD',
+    key: 'REDIS_KEY',
+  },
 };
